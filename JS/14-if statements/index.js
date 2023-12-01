f0e95e44ab647e6f912ba97560ee9a43a32424d2
IsThisDemi <furryalt68@gmail.com>
2023-12-01 21:55:45 +0100

    JS lesson 15

---
+++ JS/14-if statements/index.js
@@ -0,0 +1,31 @@
+// if statement = a basic form of decision making
+//                if a condition is true, then do something
+//                if not, then don't do it!
+
+/*
+let age = 22;
+
+if(age >= 65){
+    console.log("You are a senior!");
+}
+else if(age >= 18){
+    console.log("You are an adult!");
+
+}
+else if(age < 0){
+    console.log("You are not born yet!");
+
+}
+else{
+    console.log("You are a kid!");
+}
+*/
+
+let online = false;
+
+if(online){
+    console.log("You are online!");
+}
+else {
+    console.log("You are offline!");
+}
