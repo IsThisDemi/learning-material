d1abd48096e331a379c10fdcdcbb70a3dbad7235
IsThisDemi <furryalt68@gmail.com>
2023-12-01 18:23:02 +0100

    JS course progress, reached 10th lesson

---
+++ JS/07-Math/index.js
@@ -0,0 +1,18 @@
+
+let x = 4.21;
+let y = 5;
+let z = 9;
+let maximum;
+let minimun;
+
+// x = Math.round(x);
+// x = Math.floor(x);
+// x = Math.ceil(x);
+// x = Math.pow(x, 2);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+// maximum = Math.max(x,y,z);
+// minimun = Math.min(x,y,z);
+x = Math.PI;
+
+console.log(x);
