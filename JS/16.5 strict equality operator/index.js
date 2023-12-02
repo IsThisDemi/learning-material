3341114d29e70ea028eb09d68b48d96edd050ecc
IsThisDemi <furryalt68@gmail.com>
2023-12-02 18:58:44 +0100

    JS lesson 17

---
+++ JS/16.5 strict equality operator/index.js
@@ -0,0 +1,12 @@
+// = assignment operator
+// == comparison operator
+// === strict equality operator --> compare value and data type
+
+let x = '3.14';
+
+if (x === 3.14) {
+    console.log("That is PI!"); 
+}
+else {
+    console.log("That is NOT PI!");
+}
